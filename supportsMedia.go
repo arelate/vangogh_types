d36921d9502190f5f72700d91fcadd8eb7f4be10
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-02-28 20:40:41 -0800

    Fixing SupportsMedia and simplifying it while we're there

--- supportsMedia.go
+++ supportsMedia.go
@@ -3,30 +3,16 @@ package vangogh_types
 import "github.com/arelate/gog_types"
 
 func SupportsMedia(pt ProductType, mt gog_types.Media) bool {
-	if mt == gog_types.Unknown {
+	if !gog_types.ValidMedia(mt) {
+		return false
+	}
+	if !ValidProductType(pt) {
 		return false
 	}
 
-	switch pt {
-	case Store:
-		fallthrough
-	case Account:
-		fallthrough
-	case Wishlist:
-		fallthrough
-	case StoreProducts:
-		fallthrough
-	case AccountProducts:
-		fallthrough
-	case WishlistProducts:
-		fallthrough
-	case Details:
-		return true
-	case ApiProductsV1:
-		return true
-	case ApiProductsV2:
+	if pt == ApiProductsV2 {
 		return mt == gog_types.Game
-	default:
-		return false
 	}
+
+	return true
 }
